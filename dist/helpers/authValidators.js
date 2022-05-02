568e8de11135d6947b66804a8d0ac43196e093de
federicos <federico.scag@gmail.com>
2022-05-02 03:40:37 -0300

    error handler and db added

--- dist/helpers/authValidators.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.checkAuthenticated = void 0;
-const checkAuthenticated = (req, res, next) => {
-};
-exports.checkAuthenticated = checkAuthenticated;
-//# sourceMappingURL=authValidators.js.map
